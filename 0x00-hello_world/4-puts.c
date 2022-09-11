5d4ce2a2ece19c87d4bda1529bd9e1aaff473b79
Zimuzo <czimuzo@gmail.com>
2022-09-11 09:48:17 -0700

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,6 +8,7 @@
  * return:0
  * */
  int main (void)
-{ puts ("Programming is like building a multilingual puzzle\n");
-return (0);
+{ 
+	puts ("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
