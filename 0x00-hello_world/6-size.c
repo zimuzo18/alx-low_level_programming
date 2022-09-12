bcab7d5f97073fa0a4d967587333486ce2ef88eb
Zimuzo <czimuzo@gmail.com>
2022-09-12 09:11:11 -0700

    new commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of a char: %ld byte(s)\n", sizeof(char));
 	printf("size of an int: %ld bytes(s)\n", sizeof(int));
 	printf("size of a long: %ld byte(s)\n", sizeof(long int));
 	printf("size of a long long: %ld byte(s)\n", sizeof(long long int));
