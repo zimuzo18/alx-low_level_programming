42fb4202c5723f4700a56c45d07500ebc3ab23f1
Zimuzo <czimuzo@gmail.com>
2022-09-11 17:32:33 -0700

    new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,9 @@
  * main-prints the string usinf printf
  *
  * Description:using the main function
- * this program prints "with proper grammar, but the outcome is a piece of art,"
+ * this program prints "with proper grammar, but the outcome is a piece of art"
  * Return:0
- * */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
