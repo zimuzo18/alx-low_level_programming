a86b6029d1e26e0ee96f7283f3c17f5cb87adad0
Zimuzo <czimuzo@gmail.com>
2022-09-12 18:44:57 -0700

    new commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include  <stdio.h>
+
+/**
+ * main - prints single digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
