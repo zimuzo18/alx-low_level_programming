9e56b648d8876c7c2447f30e431b6d13ad57b9d6
Zimuzo <czimuzo@gmail.com>
2022-09-12 17:54:53 -0700

    new commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- prints alphabets in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[24] = "abdefghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 1; 1 <= 24; i++)
+
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
