55ad30b1da91502270a3aa631454c202b7b28cf7
Zimuzo <czimuzo@gmail.com>
2022-09-12 18:20:40 -0700

    new commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
