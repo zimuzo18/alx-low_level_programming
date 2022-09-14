4fec96fab325fe081bee2864efb4f0730ee38020
Zimuzo <czimuzo@gmail.com>
2022-09-13 19:05:21 -0700

    new commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabet
+ *
+ * @c: int to be used for this function
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
