63fe81be3e0b035e1e1a03d6e1532d8b51b20dc9
Zimuzo <czimuzo@gmail.com>
2022-09-13 19:30:53 -0700

    new commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
  * print_sign - shows if number is postive or negative
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0");
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		printf("-");
+		_putchar('-');
 		return (-1);
 	}
 }
